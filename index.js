9b59cbcf518348bd8791684b9a8da39073a2adde
cdfranz2 <cdfranz2@illinois.edu>
2015-10-12 12:46:07 -0500

    Front-end work

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+	response.render('pages/index');
+});
+
+app.get('/profile', function(request, response) {
+	response.render('pages/profile');
 });
 
 app.listen(app.get('port'), function() {
