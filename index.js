10fd24e63daf3db29a7d7b1dd43ec860fb17cc1e
xtxo014 <toheeb.okenla@grainger.com>
2015-10-14 17:09:26 -0500

    i'm a little less angry -toheeb

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var pg = require('pg');
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM musicx.playlist', function(err, result) {
+    client.query('SELECT * FROM test_table', function(err, result) {
       done()
         if(err) {
           console.error(err);
