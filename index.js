0262931fe7865f1fd6fbf7ffb0da78c45a018740
xtxo014 <toheeb.okenla@grainger.com>
2015-10-14 17:02:21 -0500

    random database testing probably messed a bunch of stuff up

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var app = express();
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM musicx.playlist', function(err, result) {
+      done()
+        if(err) {
+          console.error(err);
+          response.send("Error " + err );
+        }
+         else {
+          response.render('pages/db', { results: result.rows});
+        }
+    });
+  });
+});
 
 app.set('port', (process.env.PORT || 5000));
 
+// make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
+// might have to change these setting to match backbone
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
@@ -79,10 +96,12 @@ app.get('/playlists', function(request, response) {
 			]
 		}
 	]);
+	//change this to set homepage
+  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
+exports.app = app;
